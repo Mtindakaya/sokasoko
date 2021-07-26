a31e367cc027b963596c101d517f2a598d0477ed
rijkerd <richardaggrey7@gmail.com>
2021-07-26 05:25:29 +0300

    feat: ensure accountNumber on player creation

--- src/Player/player.model.js
+++ src/Player/player.model.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
+const actions = require('mongoose-rest-actions');
 
 const { Schema, model } = mongoose;
+
+const SCHEMA_OPTIONS = {
+  id: false,
+  timestamps: false,
+  toJSON: { getters: true },
+  toObject: { getters: true },
+  emitIndexErrors: true,
+};
 
 const positions = [
   'GOALKEEPER',
@@ -16,92 +25,105 @@ const positions = [
 
 const foot = ['RIGHT', 'LEFT', 'BOTH'];
 
-const PlayerSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'firstName is required'],
-    unique: true,
-    trim: true,
-    fake: {
-      generator: 'name',
-      type: 'firstName',
+const PlayerSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'firstName is required'],
+      trim: true,
+      fake: {
+        generator: 'name',
+        type: 'firstName',
+      },
     },
-  },
-  lastName: {
-    type: String,
-    required: [true, 'lastName is required'],
-    unique: true,
-    trim: true,
-    fake: {
-      generator: 'name',
-      type: 'lastName',
+    lastName: {
+      type: String,
+      required: [true, 'lastName is required'],
+      trim: true,
+      fake: {
+        generator: 'name',
+        type: 'lastName',
+      },
     },
-  },
-  phone: {
-    type: String,
-    required: true,
-    trim: true,
-    fake: {
-      generator: 'phone',
-      type: 'phoneNumber',
+    accountNumber: {
+      type: Number,
+      unique: true,
+      trim: true,
+      index: true,
     },
-  },
-  location: {
-    type: String,
-    trim: true,
-    index: true,
-    searchable: true,
-    fake: {
-      generator: 'address',
-      type: 'streetAddress',
+    phone: {
+      type: String,
+      required: [true, 'Phone Number already exists'],
+      unique: true,
+      index: true,
+      trim: true,
+      fake: {
+        generator: 'phone',
+        type: 'phoneNumber',
+      },
     },
-  },
-  dob: {
-    type: Date,
-    required: true,
-    fake: {
-      generator: 'datatype',
-      type: 'datetime',
+    location: {
+      type: String,
+      trim: true,
+      index: true,
+      searchable: true,
+      fake: {
+        generator: 'address',
+        type: 'streetAddress',
+      },
     },
-  },
-  nationality: {
-    type: String,
-    fake: {
-      generator: 'address',
-      type: 'country',
+    dob: {
+      type: Date,
+      required: true,
+      fake: {
+        generator: 'datatype',
+        type: 'datetime',
+      },
     },
+    nationality: {
+      type: String,
+      fake: {
+        generator: 'address',
+        type: 'country',
+      },
+    },
+    gender: {
+      type: String,
+      index: true,
+      searchable: true,
+      enum: ['F', 'M'],
+    },
+    weight: {
+      type: Number,
+    },
+    height: {
+      type: Number,
+      index: true,
+      searchable: true,
+    },
+    position: {
+      type: String,
+      trim: true,
+      enum: positions,
+      index: true,
+      searchable: true,
+    },
+    foot: {
+      type: String,
+      enum: foot,
+      trim: true,
+      index: true,
+      searchable: true,
+    },
+    password: { type: String },
   },
-  gender: {
-    type: String,
-    index: true,
-    searchable: true,
-    enum: ['F', 'M'],
-  },
-  weight: {
-    type: Number,
-  },
-  height: {
-    type: Number,
-    index: true,
-    searchable: true,
-  },
-  position: {
-    type: String,
-    trim: true,
-    enum: positions,
-    index: true,
-    searchable: true,
-  },
-  foot: {
-    type: String,
-    enum: foot,
-    trim: true,
-    index: true,
-    searchable: true,
-  },
-  password: { type: String },
+  SCHEMA_OPTIONS
+);
+
+PlayerSchema.pre('validate', function cb(next) {
+  next();
 });
 
-mongoose.plugin(require('@lykmapipo/mongoose-faker'));
+mongoose.plugin(actions, require('@lykmapipo/mongoose-faker'));
 
 module.exports = model('Player', PlayerSchema);
