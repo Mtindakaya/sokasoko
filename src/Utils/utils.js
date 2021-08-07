2d4c9503ad881a62a59d4e76e85674a8bcf8deda
rijkerd <richardaggrey7@gmail.com>
2021-08-07 19:20:13 +0300

    feat: add implement for password hash creation and comparison

---
+++ src/Utils/utils.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs');
+
+const generateHash = async (password, saltRounds = 10) => {
+  try {
+    //   Generate Salt
+    const salt = await bcrypt.genSalt(saltRounds);
+    const payload = await bcrypt.hash(password, salt);
+    return payload;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+const compare = async (password, hash) => {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (error) {
+    console.log(error); //eslint-disable-line
+  }
+  return false;
+};
+
+module.exports = { generateHash, compare };
