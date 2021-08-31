4c42f7c244de3a3b9d24a2894f01f5326f72e404
rijkerd <richardaggrey7@gmail.com>
2021-08-31 14:57:05 +0300

    feat: setup user login

--- src/User/user.http.router.js
+++ src/User/user.http.router.js
@@ -12,11 +12,12 @@ const { getString } = require('@lykmapipo/env');
 const _ = require('lodash');
 const { uploaderFor } = require('@lykmapipo/file');
 const Counter = require('../Counter/counter.model');
-const { leftFillNum } = require('../Utils/utils');
+const { leftFillNum, sendSms } = require('../Utils/utils');
 
 const API_VERSION = getString('API_VERSION', '1.0.0');
 const PATH_SINGLE = '/users/:id';
 const PATH_LIST = '/users';
+const PATH_LOGIN = '/users/login';
 const PATH_SCHEMA = '/users/schema/';
 
 const User = require('./user.model');
@@ -56,10 +57,25 @@ router.post(
   uploaderFor(),
   postFor({
     post: async (body, done) => {
-      const userType = _.get(body, 'type', 'P');
+      const userType = _.get(body, 'type', 'PLAYER');
       const counter = await Counter.getNextSequenceValue('memberId');
-      const accountNumber = `TFH-${userType}-A${leftFillNum(counter, 6)}`;
-      return User.post({ ...body, accountNumber }, done);
+      const accountNumber = `TFH-${userType.charAt(0)}-A${leftFillNum(
+        counter,
+        6
+      )}`;
+      User.post({ ...body, accountNumber }, (err, data) => {
+        if (err) {
+          return done(err, null);
+        }
+        const payload = data.phone.replace(data.phone.charAt(0), '255');
+        sendSms(
+          `Hello, ${(data.firstName, data.lastName)} Account Number : ${
+            data.accountNumber
+          }`,
+          payload
+        );
+        return done(null, data);
+      });
     },
   })
 );
@@ -90,5 +106,27 @@ router.delete(
     del: (options, done) => User.del(options, done),
   })
 );
+
+router.post(PATH_LOGIN, (request, response, next) => {
+  const identifier = _.get(request.body, 'identifier');
+  const password = _.get(request.body, 'password');
+
+  User.findOne({
+    $or: [{ phone: identifier }, { accountNumber: identifier }],
+  }).exec((err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    user.comparePassword(password, (error, isMatch) => {
+      if (isMatch) {
+        return response.ok(user);
+      }
+      return response.notFound();
+    });
+
+    return next;
+  });
+});
 
 module.exports = router;
