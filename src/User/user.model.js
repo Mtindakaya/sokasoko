7435e9dcd8c85318ea9f5187ee09c6f77545386a
rijkerd <richardaggrey7@gmail.com>
2021-08-31 06:32:05 +0300

    feat: implement user model

---
+++ src/User/user.model.js
@@ -0,0 +1,166 @@
+const mongoose = require('mongoose');
+const actions = require('mongoose-rest-actions');
+const bcrypt = require('bcryptjs');
+const { FileTypes } = require('@lykmapipo/file');
+
+const { generateHash, sendSms } = require('../Utils/utils');
+
+const { Schema, model } = mongoose;
+
+// mongoose.set('debug', true);
+
+const SCHEMA_OPTIONS = {
+  id: false,
+  timestamps: false,
+  toJSON: { getters: true },
+  toObject: { getters: true },
+  emitIndexErrors: true,
+};
+
+const positions = [
+  'GOALKEEPER',
+  'CENTER BACK',
+  'RIGHT BACK',
+  'LEFT BACK',
+  'WING BACK',
+  'OFFENSIVE BACK',
+  'DEFENSIVE MIDFIELD',
+  'STRIKER',
+  'WINGER',
+];
+
+const foot = ['RIGHT', 'LEFT', 'BOTH'];
+
+const types = ['P', 'C', 'G'];
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'firstName is required'],
+      searchable: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, 'lastName is required'],
+      searchable: true,
+      trim: true,
+    },
+    accountNumber: {
+      type: String,
+      unique: true,
+      trim: true,
+      index: true,
+      exists: true,
+    },
+    phone: {
+      type: String,
+      required: [true, 'Phone Number already exists'],
+      unique: true,
+      index: true,
+      trim: true,
+    },
+    region: {
+      type: String,
+      trim: true,
+      index: true,
+      searchable: true,
+      default: 'Dar es Salaam',
+    },
+    district: {
+      type: String,
+      trim: true,
+      index: true,
+      searchable: true,
+      default: 'Ilala Municipal',
+    },
+    type: { type: String, enum: types, default: types[0] },
+    ward: {
+      type: String,
+      trim: true,
+      index: true,
+      searchable: true,
+      default: 'Ilala',
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    age: { type: Number },
+    nationality: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      index: true,
+      searchable: true,
+      enum: ['FEMALE', 'MALE'],
+    },
+    weight: {
+      type: Number,
+    },
+    height: {
+      type: Number,
+      index: true,
+      searchable: true,
+    },
+    position: {
+      type: String,
+      trim: true,
+      enum: positions,
+      index: true,
+      searchable: true,
+    },
+    foot: {
+      type: String,
+      enum: foot,
+      trim: true,
+      index: true,
+      searchable: true,
+    },
+    profileImage: {
+      type: Schema.Types.ObjectId,
+      ref: FileTypes.File.ref,
+      autopopulate: true,
+    },
+    password: { type: String, required: true },
+  },
+  SCHEMA_OPTIONS
+);
+
+UserSchema.index({ firstName: 'text', lastName: 'text' });
+
+UserSchema.pre('save', function preValidate(done) {
+  return this.preValidate(done);
+});
+
+UserSchema.post('save', function postSave(done) {
+  const sender = this.phone;
+  const payload = sender.replace(sender.charAt(0), '255');
+  sendSms(`Hello, Account Number : ${this.accountNumber}`, payload);
+  return done;
+});
+
+UserSchema.methods.preValidate = async function preValidate(done) {
+  if (!this.isModified('password')) {
+    return done();
+  }
+
+  this.password = await generateHash(this.password);
+
+  return done();
+};
+
+UserSchema.methods.comparePassword = function comparePassword(password, done) {
+  bcrypt.compare(password, this.password, function cb(err, isMatch) {
+    if (err) {
+      return done(err, false);
+    }
+    return done(null, isMatch);
+  });
+};
+
+mongoose.plugin(actions);
+
+module.exports = model('User', UserSchema);
