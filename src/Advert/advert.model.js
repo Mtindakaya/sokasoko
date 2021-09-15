e1b8e06b48a6df11cb455f20a7be5a74208f745c
rijkerd <richardaggrey7@gmail.com>
2021-09-15 23:34:46 +0300

    feat: implement advert model

---
+++ src/Advert/advert.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const actions = require('mongoose-rest-actions');
+const { FileTypes } = require('@lykmapipo/file');
+
+const { Schema, model } = mongoose;
+
+const AdvertSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  image: {
+    type: Schema.Types.ObjectId,
+    ref: FileTypes.File.ref,
+    autopopulate: true,
+  },
+});
+
+mongoose.plugin(actions);
+
+module.exports = model('Advert', AdvertSchema);
