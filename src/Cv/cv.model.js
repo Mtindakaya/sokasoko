7cb14ef7ef8ac857a62edbcc2e817c313318d3de
rijkerd <richardaggrey7@gmail.com>
2021-10-31 14:40:56 +0300

    feat: :sparkles: add cv for user

---
+++ src/Cv/cv.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const CvSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    start_date: {
+      type: Date,
+      required: true,
+    },
+    end_date: {
+      type: Date,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    id: false,
+    timestamps: true,
+    toJSON: { getters: true },
+    toObject: { getters: true },
+    emitIndexErrors: true,
+  }
+);
+
+module.exports = model('Cv', CvSchema);
