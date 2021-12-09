4f79eda5b6e700cff5512fcdf9e0f523afb14df5
rijkerd <richardaggrey7@gmail.com>
2021-12-09 19:22:19 +0300

    feat: implement agent schema def

---
+++ src/Agent/agent.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const actions = require('mongoose-rest-actions');
+
+const { Schema, model } = mongoose;
+
+const AgentSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    phone: String,
+  },
+  {
+    id: false,
+    timestamps: true,
+    toJSON: { getters: true },
+    toObject: { getters: true },
+    emitIndexErrors: true,
+  }
+);
+
+AgentSchema.pre('validate', function (done) {
+  return this.preValidate(done);
+});
+
+AgentSchema.methods.preValidate = function preValidate(done) {
+  return done(null, this);
+};
+
+mongoose.plugin(actions);
+
+module.exports = model('Agent', AgentSchema);
