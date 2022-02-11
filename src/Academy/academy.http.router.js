98c5e01fcb9a4e30abb300744fff56a9107289dd
rijkerd <richardaggrey7@gmail.com>
2022-02-11 21:41:54 +0300

    feat: :sparkles: implement player to academy association

--- src/Academy/academy.http.router.js
+++ src/Academy/academy.http.router.js
@@ -9,6 +9,7 @@ const {
   schemaFor,
 } = require('@lykmapipo/express-rest-actions');
 const { getString } = require('@lykmapipo/env');
+const _ = require('lodash');
 
 const API_VERSION = getString('API_VERSION', '1.0.0');
 const PATH_SINGLE = '/academys/:id';
@@ -16,6 +17,7 @@ const PATH_LIST = '/academys';
 const PATH_SCHEMA = '/academys/schema/';
 
 const Academy = require('./academy.model');
+const User = require('../User/user.model');
 
 const router = new Router({
   version: API_VERSION,
@@ -49,7 +51,22 @@ router.post(
   PATH_LIST,
   postFor({
     post: async (body, done) => {
-      return Academy.post(body, done);
+      return Academy.post(body, (error, data) => {
+        if (error) return done(error, null);
+
+        const playerId = _.get(data, 'player._id');
+        const academyId = _.get(data, 'addedBy._id');
+
+        return User.findById(playerId, (err, player) => {
+          if (err) return done(err, null);
+
+          // eslint-disable-next-line no-param-reassign
+          player.academy = academyId;
+          player.save();
+
+          return done(null, player);
+        });
+      });
     },
   })
 );
